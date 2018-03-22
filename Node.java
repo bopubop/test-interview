34960808d7f10895917591fb4de231b9866a1c9d
bopubop <bopubop@gmail.com>
2018-03-22 12:36:40 +0200

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,18 @@
+public class Node <T>{
+    public final T value;
+    private Node<T> nextNode;
+
+    public Node(T value) {
+        this.value = value;
+    }
+
+    public Node<T> getNextNode() {
+        return nextNode;
+    }
+
+    public Node<T> setNextNode(Node<T> nextNode) {
+        this.nextNode = nextNode;
+        return nextNode;
+    }
+
+}
